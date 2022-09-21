2b80df0a5403719065b3f032b4ad6decf6a6c313
abhisonawane157 <abhisonawane157@gmail.com>
2022-09-21 19:44:53 +0530

    -

---
+++ Java_Programs/LeetCode_J.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LeetCode_J {
+    static int sumEvenAfterQueries(int[] nums) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1 };
+        int ans = sumEvenAfterQueries(nums);
+        System.out.println(ans);
+    }
+}
