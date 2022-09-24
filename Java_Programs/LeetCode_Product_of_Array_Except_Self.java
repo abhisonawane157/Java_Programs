fcea2090ae49ea3c79748b3a6b8d1cbd242fe108
abhisonawane157 <abhisonawane157@gmail.com>
2022-09-24 12:43:29 +0530

    -

--- Java_Programs/LeetCode_Product_of_Array_Except_Self.java
+++ Java_Programs/LeetCode_Product_of_Array_Except_Self.java
@@ -18,21 +18,17 @@ public class LeetCode_Product_of_Array_Except_Self {
         int[] left = new int[arr.length];
         int[] right = new int[arr.length];
         left[0] = 1;
-        int max = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++) {
             left[i] = left[i - 1] * arr[i - 1];
-            max = Math.max(max, left[i]);
         }
         right[arr.length - 1] = 1;
         for (int i = arr.length - 2; i >= 0; i--) {
             right[i] = right[i + 1] * arr[i + 1];
-            max = Math.max(max, right[i]);
         }
         int[] pdt = new int[arr.length];
         for (int i = 0; i < pdt.length; i++) {
             pdt[i] = left[i] * right[i];
         }
-        System.out.println(max);
         return pdt;
     }
 
